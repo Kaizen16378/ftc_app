fb5dcfd8510a991903f8cc89bdbe85e75d8fae52
cpucuber <42344274+cpucuber@users.noreply.github.com>
2019-07-02 11:16:40 -0400

    Update DriveTeleOpMode.java
    This is the Drive TeleOp for the Robot. It includes all the code for driving.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTeleOpMode.java
@@ -10,7 +10,9 @@ public class DriveTeleOpMode extends LinearOpMode {
     private DigitalChannel digitalTouch;
     private DistanceSensor sensorColorRange;
     private Servo servoTest;
-    leftMotor = hardwareMap.dcMotor.get("lmotor");
+    private lMotor leftMotor;
+    private rMotor rightMotor;
+
     @Override
     public void runOpMode() {
         imu = hardwareMap.get(Gyroscope.class, "imu");
@@ -18,17 +20,40 @@ public class DriveTeleOpMode extends LinearOpMode {
         digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
         sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
         servoTest = hardwareMap.get(Servo.class, "servoTest");
+        leftMotor = hardwareMap.dcMotor.get(lMotor.class, "leftMotor");
+        rightMotor = hardwareMap.dcMotor.get(rMotor.class, "rightMotor");
         telemetry.addData("Status", "Initialized");
         telemetry.update();
+
+        //Drive Code
+        boolean drivemode = true;
         double throttle = gamepad1.left_stick_y;
         double turn =  gamepad1.left_stick_x;
 
         double leftspeed = throttle - turn;
         double rightspeed = throttle + turn;
+
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
+       
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+            
+            //Drive Mode Switching
+            if (gamepad1.dpad_down){
+                drivemode = (drivemode) ? false : true;
+            }
+            
+            if(drivemode) {
+                leftspeed = throttle - turn;
+                rightspeed = throttle + turn;
+            }
+            else{
+                leftspeed = gamepad1.left_stick_y;
+                rightspeed = gamepad1.right_stick_y;
+            }
+            
+            //Motor Power
             leftMotor.setPower(leftspeed);
             rightMotor.setPower(rightspeed);
             telemetry.addData("Status", "Running");
